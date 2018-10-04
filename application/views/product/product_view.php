cfd2681b0d3d2ce634ba26cc57f02626f2960e16
FungCheung <cyfung1991@gmail.com>
2018-10-04 16:49:36 +1000

    view product page update format

--- application/views/product/product_view.php
+++ application/views/product/product_view.php
@@ -4,7 +4,7 @@
    </head>
    <body>
      <?php if( count($product_info) > 0 ){
-       echo "<img src=".$this->config->item('upload_http').$product_info['pic_path'].">";
+       echo "<img src=".$this->config->item('upload_http').$product_info['pic_path']."><br>";
        echo $product_info['name']."<br>";
        echo $product_info['price']."<br>";
        echo $product_info['desc']."<br>";
